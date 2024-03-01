a0aedfff74948ab6e0dae7dc1a8211adc8c1cf08
Maximilian Rosenmüller <maximilian.rosenmueller@trustcaptcha.com>
2024-03-01 12:35:03 +0100

    Fix comments in main.py

--- samples/backends/python-sample/main.py
+++ samples/backends/python-sample/main.py
@@ -14,8 +14,8 @@ def post_api_example():
     # Retrieving the verification result
     try:
         verification_result = CaptchaManager.get_verification_result("<your_secret_key>", verification_token)
-    except Exception as e:  # Hier könnten Sie eine spezifischere Ausnahme einfügen
-        # Log the exception or handle it
+    except Exception as e:
+        # Fetch verification result failed - handle error
         print(f"Failed to fetch verification result: {e}")
         return jsonify({'error': 'Captcha verification failed'}), 500
 
